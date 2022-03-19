cd78a1e3a96e33c1f713aa8da73f419d9b422911
BoKyung <leebk1124@gmail.com>
2022-03-19 22:31:02 +0900

    Initial commit

---
+++ app/src/main/java/com/example/registerloginpage/data/RecommendDatasource.kt
@@ -0,0 +1,14 @@
+package com.example.registerloginpage.data
+
+import com.example.registerloginpage.model.ForMe
+import com.example.registerloginpage.R
+
+class RecommendDatasource {
+    fun loadRecommend(): List<ForMe> {
+        return listOf<ForMe>(ForMe(R.string.recommend1, R.drawable.recommend1),
+            ForMe(R.string.recommend2, R.drawable.recommend2),
+            ForMe(R.string.recommend3, R.drawable.recommend3),
+            ForMe(R.string.recommend4, R.drawable.recommend4),
+            ForMe(R.string.recommend5, R.drawable.recommend5) )
+    }
+}
